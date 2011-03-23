357391ecc5f5b79c26830acfb4b2435f8f403532
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-03-23 22:06:33 +0100

    add example session for wget

---
+++ wget/wget.py
@@ -0,0 +1,8 @@
+from jam.session import ConfigureSession
+
+class Wget(ConfigureSession):
+
+    url = "http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2"
+    hash = { "308a5476fc096a8a525d07279a6f6aa3": "md5" }
+    version = "1.12"
+
