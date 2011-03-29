5feec3a1a2dc0fc4977eff3f855ef3dcbc2ac9a5
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-03-29 17:58:12 +0200

    change order of hash type and hash value. hash type must be first

--- wget/wget.py
+++ wget/wget.py
@@ -3,6 +3,6 @@ from jam.session import ConfigureSession
 class Wget(ConfigureSession):
 
     url = "http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2"
-    hash = { "308a5476fc096a8a525d07279a6f6aa3": "md5" }
+    hash = { "md5" : "308a5476fc096a8a525d07279a6f6aa3" }
     version = "1.12"
 
