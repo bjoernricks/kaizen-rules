287639704df68bb2ed3d9d8cc1022172806eab00
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-03-29 18:00:35 +0200

    don't use from imports. this results in loading the wrong session class by the session loader

--- wget/wget.py
+++ wget/wget.py
@@ -1,6 +1,6 @@
-from jam.session import ConfigureSession
+import jam.session
 
-class Wget(ConfigureSession):
+class Wget(jam.session.ConfigureSession):
 
     url = "http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2"
     hash = { "md5" : "308a5476fc096a8a525d07279a6f6aa3" }
