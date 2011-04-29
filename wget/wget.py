87701e51e65d5944a58be8aad80350780a7ccf07
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-04-29 19:59:07 +0200

    set name for wget session

--- wget/wget.py
+++ wget/wget.py
@@ -5,4 +5,5 @@ class Wget(jam.session.ConfigureSession):
     url = "http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2"
     hash = { "md5" : "308a5476fc096a8a525d07279a6f6aa3" }
     version = "1.12"
+    name = "wget"
 
