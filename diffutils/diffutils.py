8b7b47b3a78a7bbb3a718b106218572015fc1932
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-07-08 19:27:22 +0200

    Add session for gnu diffutils

---
+++ diffutils/diffutils.py
@@ -0,0 +1,10 @@
+import jam.session
+
+class Diffutils(jam.session.ConfigureSession):
+
+    url = "http://ftp.gnu.org/gnu/diffutils/diffutils-3.0.tar.gz"
+    hash = { "md5" : "684aaba1baab743a2a90e52162ff07da",
+             "sha1" : "17fcdcd435ef6b424aa9c7a487ffde408d1a00e6" }
+    version = "3.0"
+    name = "diffutils"
+
