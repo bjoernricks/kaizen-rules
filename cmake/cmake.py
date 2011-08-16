2c28c85d2ec58f36850f13eb095d4a3159b56e56
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-08-16 21:41:12 +0200

    Add session for cmake

---
+++ cmake/cmake.py
@@ -0,0 +1,25 @@
+import jam.session
+
+class Cmake(jam.session.ConfigureSession):
+
+    url = "http://www.cmake.org/files/v2.8/cmake-2.8.5.tar.gz"
+    hash = { "md5" : "3c5d32cec0f4c2dc45f4c2e84f4a20c5",
+             "sha1" : "8dd4c31cbccf1a297829a476a0ef79d1614ca368" }
+    version = "2.8.5"
+    name = "cmake"
+
+    # uncomment to set path to source directory
+    # default is %(src_dir)s/%(name)s-%(version)s
+    # src_path = ""
+
+    # uncomment to set path to build directory (normally not necessary)
+    # build_path = ""
+
+    args = ["--mandir=/share/man", "--docdir=/share/doc/cmake",
+            "--system-libs", "--datadir=/share/cmake", "--no-system-libarchive"]
+
+    # uncomment to add additonal patches 
+    # all patches will be copied to %(downloadroot)s/%(session)s/patches
+    # e.g. patches = ["01-patch.diff", "http://url.com/remotepatch.diff"]
+    # patches = [""]
+
