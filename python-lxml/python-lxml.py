3439186e1fcb6fe403ee95c78cf8a206bcf34f68
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-09-03 18:55:16 +0200

    Add a new session for lxml (python libxml bindings)

---
+++ python-lxml/python-lxml.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythonlxml(jam.session.PythonSession):
+
+    url = "http://lxml.de/files/lxml-2.3.tgz"
+    hash = { "md5" : "a245a015fd59b63e220005f263e1682a",
+             "sha1" : "2384caa37b94b312d97d3e4b773aeccca94f9c76" }
+    version = "2.3"
+    name = "lxml"
