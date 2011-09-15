1f868d0dbc63f16f8d733ef31f7b84bff8468b01
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-09-15 14:57:26 +0200

    Add session for python distribute

---
+++ python-distribute/python-distribute.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythondistribute(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/d/distribute/distribute-0.6.21.tar.gz"
+    hash = { "md5" : "c8cfcfd42ec9ab900fb3960a3308eef2",
+             "sha1" : "4bc3df524ba1d684ba997caab3346040551a3820" }
+    version = "0.6.21"
+    name = "distribute"
