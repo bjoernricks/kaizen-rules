27b60b907a4f8cd79f93adf24c8b1d1ef7b2f69a
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-09-22 11:52:45 +0200

    Add session for pylons

---
+++ python-pylons/python-pylons.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythonpylons(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/P/Pylons/Pylons-1.0.tar.gz"
+    hash = { "md5" : "b7687e26d0275eaf7bf44ca4883f4428",
+             "sha1" : "a624fc3c4ef3ab382a23f460b030e331c4320ca0" }
+    version = "1.0"
+    name = "Pylons"
