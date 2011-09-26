d1d2228cad231c328b997a1c5b67217037e54261
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-09-26 13:34:04 +0200

    add session for python webob

---
+++ python-webob/python-webob.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Pythonwebob(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/W/WebOb/WebOb-%(version)s.tar.gz"
+    hash = { "md5" : "07d1a1a4b0bf0faa67cb6638c632ea61",
+             "sha1" : "32793f14b1d63af3ce3ed965b214e4ce69404863" }
+    version = "0.9.8"
+    name = "WebOb"
+
+    depends = ["python-distribute"]
