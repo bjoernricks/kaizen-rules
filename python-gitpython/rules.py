6afc8cc82a5c99c00d2e51cf0ce6432f4f5248a0
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-09-27 18:55:36 +0200

    Add session for gitpython

---
+++ python-gitpython/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythongitpython(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/G/GitPython/GitPython-%(version)s.tar.gz"
+    hash = { "md5" : "849082fe29adc653a3621465213cab96",
+             "sha1" : "b9f43c91452f3fe7e105ac54ce878ff20ea44f44" }
+    version = "0.3.2.RC1"
+    name = "GitPython"
