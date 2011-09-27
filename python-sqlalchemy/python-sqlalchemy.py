24883aa6936029397f1e2a82ab11bb5775c2e660
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-09-27 18:32:10 +0200

    Add session for sqlalchemy

---
+++ python-sqlalchemy/python-sqlalchemy.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythonsqlalchemy(jam.session.PythonSession):
+
+    url = "http://downloads.sourceforge.net/project/sqlalchemy/sqlalchemy/0.7.2/SQLAlchemy-%(version)s.tar.gz"
+    hash = { "md5" : "234494800f2a0ac3ad7c06e0ffa4641e",
+             "sha1" : "e41e822ba4b728ae139532d84bbb8b5043fb9116" }
+    version = "0.7.2"
+    name = "SQLAlchemy"
