8001d863047ea6d5187770f0196acd87ab601926
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-09-29 20:30:29 +0200

    Add session for libjpeg

---
+++ libjpeg/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Libjpeg(jam.session.ConfigureSession):
+
+    url = "http://www.ijg.org/files/jpegsrc.v8c.tar.gz"
+    hash = { "md5" : "a2c10c04f396a9ce72894beb18b4e1f9",
+             "sha1" : "f0a3b88ac4db19667798bee971537eeed552bce9" }
+    version = "8c"
+    name = "jpeg"
