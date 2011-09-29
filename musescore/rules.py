5d8a4da447122a54d00a1cb561d93aa3d03936f9
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-09-29 20:31:00 +0200

    Add session for musecore

---
+++ musescore/rules.py
@@ -0,0 +1,27 @@
+import jam.session
+
+class Mscore(jam.session.Session):
+
+    url = "http://sourceforge.net/projects/mscore/files/mscore/MuseScore-1.1/mscore-1.1.tar.bz2"
+    hash = { "md5" : "68b43af92093a16f7f074c0eb560a867",
+             "sha1" : "a4e3963b995bbadf0f9a9ecdb6134d58414fd6cc" }
+    version = "1.1"
+    name = "mscore"
+
+    # uncomment to set path to source directory
+    # default is %(src_dir)s/%(name)s-%(version)s
+    # src_path = ""
+
+    # uncomment to set path to build directory (normally not necessary)
+    # build_path = ""
+
+    # uncomment to pass additonal parameters to configure script
+    # args = [""]
+
+    # uncomment to add additonal patches 
+    # all patches will be copied to %(downloadroot)s/%(session)s/patches
+    # e.g. patches = ["01-patch.diff", "http://url.com/remotepatch.diff"]
+    # patches = [""]
+
+    # uncomment to add a dependency on an other session
+    depends = ["qt4"]
