f822cfa0381236ed0b753e57c14c1e1985ce7401
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-10-10 14:23:35 +0200

    Add session for urwid

---
+++ python-urwid/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythonurwid(jam.session.PythonSession):
+
+    url = "http://excess.org/urwid/urwid-1.0.0.tar.gz"
+    hash = { "md5" : "487e98c5aef14af3b95ed2afa6147dd9",
+             "sha1" : "a7b9b1f60706cc44a71d81dc1bff38b268cffde2" }
+    version = "1.0.0"
+    name = "urwid"
