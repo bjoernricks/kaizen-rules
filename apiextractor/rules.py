5a4812b826d78989d7d2e1ed514f9afaa599a7f4
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-01 22:02:19 +0100

    Add session for apiextractor

---
+++ apiextractor/rules.py
@@ -0,0 +1,13 @@
+import jam.session
+
+class Apiextractor(jam.session.CMakeSession):
+
+    url = "http://www.pyside.org/files/%(name)s-%(version)s.tar.bz2"
+    hash = { "md5" : "89a3dd539e98fccd0b3f8da881f60395",
+             "sha1" : "da6bc41731d3f650e4cab4d0f9be7c72875106f4" }
+    version = "0.10.9"
+    name = "apiextractor"
+
+    depends = ["python", "qt4", "libxml2", "libxslt"]
+
+    configure_args = ["-DBUILD_TESTS=FALSE"]
