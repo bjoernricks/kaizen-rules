56786a8451848192d8b8d0f96c9586b297fc6a6a
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-01 22:04:32 +0100

    add session for pyside-tools

---
+++ pyside-tools/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Pysidetools(jam.session.CMakeSession):
+
+    url = "http://www.pyside.org/files/pyside-tools-0.2.13.tar.bz2"
+    hash = { "md5" : "14d3a36df06d680357d7bc1960f19a6d",
+             "sha1" : "4d05444300331518c3b66536aec3048454db3380" }
+    version = "0.2.13"
+    name = "pyside-tools"
+
+    depends = ["python", "python-pyside", "qt4"]
