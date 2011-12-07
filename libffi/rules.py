048566290aa4a0a79321e07441b4245604377659
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-07 23:43:25 +0100

    Add session for libffi
    libffi is installed on mac ox but glib can't find it ...

---
+++ libffi/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Libffi(jam.session.ConfigureSession):
+
+    url = "ftp://sourceware.org/pub/libffi/libffi-3.0.10.tar.gz"
+    hash = { "md5" : "79390673f5d07a8fb342bc09b5055b6f",
+             "sha1" : "97abf70e6a6d315d9259d58ac463663051d471e1" }
+    version = "3.0.10"
+    name = "libffi"
