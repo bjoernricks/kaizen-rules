dd2936cb7864e58b31e6bd70b195033f2772a412
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-07 22:44:22 +0100

    Add session for pkg-config

---
+++ pkg-config/rules.py
@@ -0,0 +1,12 @@
+import jam.session
+
+class Pkgconfig(jam.session.ConfigureSession):
+
+    url = "http://pkg-config.freedesktop.org/releases/pkg-config-%(version)s.tar.gz"
+    hash = { "md5" : "a3270bab3f4b69b7dc6dbdacbcae9745",
+             "sha1" : "8922aeb4edeff7ed554cc1969cbb4ad5a4e6b26e" }
+    version = "0.25"
+    name = "pkg-config"
+
+    configure_args = \
+    ["--with-pc-path=%(prefix)s/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig/"]
