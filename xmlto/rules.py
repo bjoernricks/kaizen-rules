ff9002474af3d309f7bf96eccbf77f094e05ba6d
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-12 19:45:41 +0100

    Add session for xmlto

---
+++ xmlto/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Xmlto(jam.session.ConfigureSession):
+
+    url = "https://fedorahosted.org/releases/x/m/xmlto/xmlto-%(version)s.tar.bz2"
+    hash = { "md5" : "6b6267b1470f8571fe5f63a128970364",
+             "sha1" : "5d1aecd59d519066f94b4591722767c4e41bdc0f" }
+    version = "0.0.25"
+    name = "xmlto"
+
+    depends = ["getopt", "libxml2"]
