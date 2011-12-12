0636869c47b48ea64a613881a09e06bcc8e96724
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-12 19:41:49 +0100

    Fix asciidoc session

--- asciidoc/rules.py
+++ asciidoc/rules.py
@@ -1,9 +1,17 @@
 import jam.session
 
+from jam.system import Copy
+
 class Asciidoc(jam.session.ConfigureSession):
 
-    url = "http://downloads.sourceforge.net/project/asciidoc/asciidoc/%(version)s/%(version)s-8.6.6.tar.gz"
+    url = "http://downloads.sourceforge.net/project/asciidoc/asciidoc/%(version)s/asciidoc-%(version)s.tar.gz"
     hash = { "md5" : "44b872d9c300ffa5a8fe8b3c4d10957c",
              "sha1" : "f43f96d8599c3337baf12df18315ecc0b9e885c7" }
     version = "8.6.6"
     name = "asciidoc"
+
+    def pre_configure(self):
+        Copy(self.src_path, self.build_path).run()
+
+    def distclean(self):
+        pass
