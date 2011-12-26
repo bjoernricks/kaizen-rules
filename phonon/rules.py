921a4c0c7203588b8cf96d1ff104b309b09d9db4
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-26 20:45:03 +0100

    Add session for phonon
    Also add the phonon source because they are only available in xz format
    but not in bzip2, gz or zip. xz is not supported by python.

---
+++ phonon/rules.py
@@ -0,0 +1,13 @@
+import jam.session
+
+class Phonon(jam.session.CMakeSession):
+
+    url = "phonon-4.6.0.tar.bz2"
+    hash = { "md5" : "7b194cc3a5583c958f81bb41e0f044cb",
+             "sha1" : "dd7aabe789144d2263b596873eb34b57cc3a16a2" }
+    version = "4.6.0"
+    name = "phonon"
+
+    depends = ["qt4",
+               "automoc4",
+               ]
