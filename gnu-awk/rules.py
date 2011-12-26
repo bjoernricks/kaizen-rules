a8714a0af6fd5eef21caefe585dc3ef811fa8834
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-12-26 20:46:16 +0100

    Add session for gnu-awk
    gnu-awk is required by virtuoso

---
+++ gnu-awk/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Gawk(jam.session.ConfigureSession):
+
+    url = "http://ftp.gnu.org/gnu/gawk/gawk-4.0.0.tar.bz2"
+    hash = { "md5" : "7cdc48e99b885a4bbe0e98dcf1706b22",
+             "sha1" : "9e1b7d86b5e80c85e699c269d59d6711753c51d1" }
+    version = "4.0.0"
+    name = "gawk"
+
+    depends = ["libiconv", "gettext"]
