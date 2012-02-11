2d97b6dd55b635e0c523bf215c10a9f909d633fb
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:10:24 +0100

    Add session for docbook-xsl

---
+++ docbook-xsl/rules.py
@@ -0,0 +1,47 @@
+import jam.session
+
+from jam.system import Copy, Mkdirs
+
+class Docbookxsl(jam.session.Session):
+
+    url = "http://downloads.sourceforge.net/project/docbook/docbook-xsl/%(version)s/docbook-xsl-%(version)s.tar.bz2"
+    hash = { "md5" : "b5340507cb240cc7ce00632b9c40bff5",
+             "sha1" : "dc9fa422c53e0a4f0e32b5c8ec896b39080bc14d" }
+    version = "1.76.1"
+    name = "docbook-xsl"
+
+    def destroot(self):
+        files = ["catalog.xml",
+                 "VERSION"]
+
+        dirs = ["common",
+                "docsrc",
+                "eclipse",
+                "epub",
+                "extensions",
+                "fo",
+                "highlighting",
+                "html",
+                "htmlhelp",
+                "images",
+                "javahelp",
+                "lib",
+                "manpages",
+                "params",
+                "profiling",
+                "roundtrip",
+                "slides",
+                "template",
+                "tools",
+                "webhelp",
+                "website",
+                "xhtml",
+                "xhtml-1_1",
+                ]
+
+        dest_path = self.dest_path + "/share/xsl/" + self.name
+        Mkdirs(dest_path).run()
+        for item in files:
+            Copy(self.src_path + "/" + item, dest_path).run()
+        for dir in dirs:
+            Copy(self.src_path + "/" + dir, dest_path + "/" + dir).run()
