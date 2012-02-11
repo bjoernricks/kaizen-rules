28a6671f7500e45e20417efc2b867427756e6451
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 11:19:57 +0100

    Add a session for clucene

---
+++ clucene/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Clucenecore(jam.session.CMakeSession):
+
+    url = "http://downloads.sourceforge.net/project/clucene/clucene-core-stable/%(version)s/clucene-core-%(version)s.tar.bz2"
+    hash = { "md5" : "ba1a8f764a2ca19c66ad907dddd88352",
+             "sha1" : "8bc505b64f82723c2dc901036cb0607500870973" }
+    version = "0.9.21b"
+    name = "clucene-core"
+
+    patches = ["patch-CMakeLists.txt.diff"]
