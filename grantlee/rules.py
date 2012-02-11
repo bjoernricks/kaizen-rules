4ec32ca173a3929b31fdb9a4217b88d59f141593
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:10:58 +0100

    Add session for grantlee

---
+++ grantlee/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Grantlee(jam.session.CMakeSession):
+
+    url = "http://downloads.grantlee.org/grantlee-%(version)s.tar.gz"
+    hash = { "md5" : "0622ed30630a44b5fac0bfa7e176d3fe",
+             "sha1" : "25560082ce53468fae227217d5da297dbcb9c16f" }
+    version = "0.2.0"
+    name = "grantlee"
+
+    depends = ["qt4", "automoc4"]
