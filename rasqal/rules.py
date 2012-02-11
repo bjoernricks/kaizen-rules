188094b7087ee2799b7c5942dec4bf6cc73dc98b
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 11:21:00 +0100

    Add a session for rasqal

---
+++ rasqal/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Rasqal(jam.session.ConfigureSession):
+
+    url = "http://download.librdf.org/source/rasqal-%(version)s.tar.gz"
+    hash = { "md5" : "a3662b8d9efef9d8ef0a3c182450fba2",
+             "sha1" : "9a685d7aa0a1de59e7547c1426fd8b4713fdbd9a" }
+    version = "0.9.28"
+    name = "rasqal"
+
+    depends = ["libxml2", "pkg-config", "libxslt", "raptor2"]
