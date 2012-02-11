71d7abac34e9a72eeca3e7a641f53f71ac9b4dd4
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 11:21:26 +0100

    Add a session for soprano

---
+++ soprano/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Soprano(jam.session.CMakeSession):
+
+    url = "http://downloads.sourceforge.net/project/soprano/Soprano/%(version)s/soprano-%(version)s.tar.bz2"
+    hash = { "md5" : "783fb07f9679f45e987aff7a17bef649",
+             "sha1" : "d16231dc021bd66eebb9d1975ba075d2169a2617" }
+    version = "2.7.4"
+    name = "soprano"
+
+    depends = ["raptor2", "rasqal", "redland", "clucene", "qt4", "libiodbc"]
