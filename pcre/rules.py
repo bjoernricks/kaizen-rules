517e1df33ea438f07b5049885a7cb02efaad3440
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:11:47 +0100

    Add session for pcre

---
+++ pcre/rules.py
@@ -0,0 +1,16 @@
+import jam.session
+
+class Pcre(jam.session.CMakeSession):
+
+    url = "http://downloads.sourceforge.net/project/pcre/pcre/%(version)s/pcre-%(version)s.tar.bz2"
+    hash = { "md5" : "0a7b592bea64b7aa7f4011fc7171a730",
+             "sha1" : "52abf655d94f5208377258ffff27c7b35c53af39" }
+    version = "8.21"
+    name = "pcre"
+
+    depends = ["bzip2", "zlib"]
+
+    configure_args = ["--enable-utf8",
+                      "--enable-unicode-properties",
+                      "--enable-pcregrep-libz",
+                      "--enable-pcregrep-libbz2"]
