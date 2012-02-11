b392c831a7b9f025bc948eb96421945ae1ef178e
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:11:59 +0100

    add session for libtiff

---
+++ libtiff/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Tiff(jam.session.ConfigureSession):
+
+    url = "http://download.osgeo.org/libtiff/tiff-%(version)s.tar.gz"
+    hash = { "md5" : "8fc7ce3b4e1d0cc8a319336967815084",
+             "sha1" : "f40aab20fb2f609b5cbc1171c40b66a1445e3773" }
+    version = "3.9.5"
+    name = "tiff"
