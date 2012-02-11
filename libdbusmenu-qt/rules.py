841e88fd0bd8a849d8aecf01491ddfb10311304a
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:11:32 +0100

    add session for libdbusmenu-qt

---
+++ libdbusmenu-qt/rules.py
@@ -0,0 +1,13 @@
+import jam.session
+
+class Libdbusmenuqt(jam.session.CMakeSession):
+
+    url = "http://launchpad.net/libdbusmenu-qt/trunk/%(version)s/+download/libdbusmenu-qt-%(version)s.tar.bz2"
+    hash = { "md5" : "cc6b7b551377e2a07f6fa5afef0d29ff",
+             "sha1" : "690a885825d45579607f3d6708aad84b6f3ecdd0" }
+    version = "0.9.0"
+    name = "libdbusmenu-qt"
+
+    depends = ["qt4"]
+
+    configure_args = ["-DWITH_DOC=OFF"]
