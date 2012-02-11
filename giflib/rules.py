3010bb84d7fff2bcdd26844524740e01d3d30677
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-11 17:10:40 +0100

    Add session for giflib

---
+++ giflib/rules.py
@@ -0,0 +1,10 @@
+import jam.session
+
+class Giflib(jam.session.ConfigureSession):
+
+    url = "http://downloads.sourceforge.net/project/giflib/giflib%%204.x/giflib-%(version)s/giflib-%(version)s.tar.bz2"
+    hash = { "md5" : "7125644155ae6ad33dbc9fc15a14735f",
+             "sha1" : "22680f604ec92065f04caf00b1c180ba74fb8562" }
+    version = "4.1.6"
+    name = "giflib"
+
