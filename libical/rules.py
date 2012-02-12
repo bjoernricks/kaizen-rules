6ce39a4b34d2c54c43063c34d803ffe68bcb19f9
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-12 13:28:29 +0100

    Add session from libical

---
+++ libical/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Libical(jam.session.CMakeSession):
+
+    url = "http://downloads.sourceforge.net/project/freeassociation/libical/libical-%(version)s/libical-%(version)s.tar.gz"
+    hash = { "md5" : "e549f434d5fbf9cd156c60ed4943618f",
+             "sha1" : "4693cd0438be9f3727146ac1a46aa5b1b93b8c86" }
+    version = "0.48"
+    name = "libical"
+
+    parallel = False
