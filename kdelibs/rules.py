ef657c59ce84cabad537aa29243002cad367c426
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-12 13:25:19 +0100

    Use version for url

--- kdelibs/rules.py
+++ kdelibs/rules.py
@@ -2,7 +2,7 @@ import jam.session
 
 class Kdelibs(jam.session.CMakeSession):
 
-    url = "ftp://ftp.kde.org/pub/kde/stable/4.8.0/src/kdelibs-4.8.0.tar.bz2"
+    url = "ftp://ftp.kde.org/pub/kde/stable/%(version)s/src/kdelibs-%(version)s.tar.bz2"
     hash = { "md5" : "c19858c68f9a209ae521d7fb3c34747b",
              "sha1" : "5e4744405734e6c3ce572ef7d16054390692b38a" }
     version = "4.8.0"
