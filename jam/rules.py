ea5da6d063cf9eb5275c4ccedb70b2b15caad11c
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-12 13:23:08 +0100

    Install bin/jam jam script to bin dir
    Don't use the script generated from distutils to install jam script into
    the bin dir. Using an own script allows to fix the nasty import problem
    when the current dir contains a jam directory with an __init__.py file
    e.g. within the jam sessions dir.

--- jam/rules.py
+++ jam/rules.py
@@ -1,5 +1,7 @@
 import jam.session
 
+from jam.system import Copy, Mkdirs
+
 class Jam(jam.session.PythonDevelopSession):
 
     src_path = "%(package_path)s"
@@ -8,3 +10,8 @@ class Jam(jam.session.PythonDevelopSession):
     name = "jam"
 
     runtime_depends = ["python-bz2file"]
+
+    def post_destroot(self):
+        dest_path = self.dest_path + "/bin"
+        Mkdirs(dest_path).run()
+        Copy(self.package_path + "/bin/jam", dest_path).run()
