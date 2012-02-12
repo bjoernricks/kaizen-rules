2303bab5c6397bb2af61935098646fef55d991dd
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-12 13:29:32 +0100

    Add session for kdepimlibs

---
+++ kdepimlibs/rules.py
@@ -0,0 +1,18 @@
+import jam.session
+
+class Kdepimlibs(jam.session.CMakeSession):
+
+    url = "ftp://ftp.kde.org/pub/kde/stable/4.8.0/src/kdepimlibs-4.8.0.tar.bz2"
+    hash = { "md5" : "3e1ea1d5f56eb87c0c305d941ac414c0",
+             "sha1" : "22409015a8047e3a78711093e3363775e8434fba" }
+    version = "4.8.0"
+    name = "kdepimlibs"
+
+
+    depends = ["qt4", "automoc4", "kdelibs", "gpgme", "cyrus-sasl", "akonadi",
+               "shared-mime-info", "libical"]
+
+    configure_args = [
+                      "-DBUNDLE_INSTALL_DIR=%(apps_dir)s/KDE4",
+                      "-DKDE_DISTRIBUTION_TEXT=\"jam software manager\"",
+                      ]
