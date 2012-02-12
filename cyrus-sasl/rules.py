cce672e7626b7d98e9da6e5d67a25bc88b813449
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-02-12 13:28:05 +0100

    Add Session fro cyrus-sasl

---
+++ cyrus-sasl/rules.py
@@ -0,0 +1,26 @@
+import jam.session
+
+class Cyrussasl(jam.session.ConfigureSession):
+
+    url = "ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-2.1.25.tar.gz"
+    hash = { "md5" : "341cffe829a4d71f2a6503d669d5a946",
+             "sha1" : "b6c34426012d9b5d448d5646cbecd818a5eeacbf" }
+    version = "2.1.25"
+    name = "cyrus-sasl"
+
+    depends = ["openssl"]
+
+    parallel = False
+
+    patches = ["patch-config_ltconfig.diff"]
+
+    configure_args = ["--with-dbpath=%(prefix)s/etc/sasldb2",
+                      "--with-plugindir=%(prefix)s/lib/sasl2",
+                      "--with-saslauthd=%(prefix)s/var/state/saslauthd",
+                      "--with-pwcheck=%(prefix)s/var/pwcheck",
+                      "--disable-macos-framework",
+                      "--enable-srp",
+                      "--enable-srp-setpass",
+                      "--enable-login",
+                      "--enable-ntlm",
+                      "--with-rc4=openssl"]
