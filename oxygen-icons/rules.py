03f213edfa6e519ac99ed350cbc6c38f8258cb30
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:03:02 +0100

    Add session for oxygen-icons

---
+++ oxygen-icons/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Oxygenicons(jam.session.CMakeSession):
+
+    url = "ftp://ftp.kde.org/pub/kde/stable/%(version)s/src/oxygen-icons-%(version)s.tar.bz2"
+    hash = { "md5" : "2ae26ba235f207eb29677637c1d059a7",
+             "sha1" : "cd893bb4060957e4ceff39c46e01cec82746163e" }
+    version = "4.8.0"
+    name = "oxygen-icons"
