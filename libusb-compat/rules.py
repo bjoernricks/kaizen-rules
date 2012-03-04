58a509ecf25ad7a2727fc13adf26ad122b738589
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:07:17 +0100

    add session for libusb-compat

---
+++ libusb-compat/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Libusbcompat(jam.session.ConfigureSession):
+
+    url = "http://downloads.sourceforge.net/project/libusb/libusb-compat-0.1/libusb-compat-%(version)s/libusb-compat-%(version)s.tar.bz2"
+    hash = { "md5" : "570ac2ea085b80d1f74ddc7c6a93c0eb",
+             "sha1" : "d5710d5bc4b67c5344e779475b76168c7ccc5e69" }
+    version = "0.1.3"
+    name = "libusb-compat"
+
+    depends = ["libusb"]
