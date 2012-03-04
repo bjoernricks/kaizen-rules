f5b41b76bed29115271538d80d110ab36c83d69a
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:05:08 +0100

    Add session for gdbm

---
+++ gdbm/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Gdbm(jam.session.ConfigureSession):
+
+    url = "ftp://ftp.gnu.org/gnu/gdbm/gdbm-%(version)s.tar.gz"
+    hash = { "md5" : "88770493c2559dc80b561293e39d3570",
+             "sha1" : "441201e9145f590ba613f8a1e952455d620e0860" }
+    version = "1.10"
+    name = "gdbm"
+
+    depends = ["gettext"]
