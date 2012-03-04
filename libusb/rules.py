7f6a72557e5550e5c7837b0522e91ed098178180
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:07:03 +0100

    Add session for libusb

---
+++ libusb/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Libusb(jam.session.ConfigureSession):
+
+    url = "http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-%(version)s/libusb-%(version)s.tar.bz2"
+    hash = { "md5" : "37d34e6eaa69a4b645a19ff4ca63ceef",
+             "sha1" : "5484397860f709c9b51611d224819f8ed5994063" }
+    version = "1.0.8"
+    name = "libusb"
