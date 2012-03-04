6ce20b7d9ff65e5b07bd4f4850767935b22a56f9
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 17:59:23 +0100

    Set install name path for generatorrunner

--- generatorrunner/rules.py
+++ generatorrunner/rules.py
@@ -1,5 +1,7 @@
 import jam.session
 
+from jam.system import Quilt
+
 class Generatorrunner(jam.session.CMakeSession):
 
     url = "http://www.pyside.org/files/%(name)s-%(version)s.tar.bz2"
@@ -9,5 +11,7 @@ class Generatorrunner(jam.session.CMakeSession):
     name = "generatorrunner"
 
     depends = ["qt4", "apiextractor"]
+
+    patchsystem = Quilt
 
     configure_args = ["-DBUILD_TESTS=FALSE"]
