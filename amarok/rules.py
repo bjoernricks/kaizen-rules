1839dd4c0b350b0b33d30fa949642e5f27d79ac7
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:08:36 +0100

    Add session for amarok

---
+++ amarok/rules.py
@@ -0,0 +1,24 @@
+import jam.session
+
+class Amarok(jam.session.CMakeSession):
+
+    url = "ftp://ftp.kde.org/pub/kde/stable/amarok/2.5.0/src/amarok-2.5.0.tar.bz2"
+    hash = { "md5" : "b7983eaa33e4771769ae9e330c811995",
+             "sha1" : "9849900d20225e703c43d242650a8fa211cf15f2" }
+    version = "2.5.0"
+    name = "amarok"
+
+    # configure_args = [""]
+
+    depends = ["taglib",
+               "kdelibs",
+               "kde-runtime",
+               "qtscriptgenerator",
+               "qca",
+               "libmtp",
+               "libgpod",
+               "mysql",
+               "phonon",
+               "zlib",
+               "taglib-extras",
+               ]
