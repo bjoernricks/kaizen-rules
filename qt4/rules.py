4bbf5f79a656a19cce1a4047ac0ff5844909e342
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-03-04 18:03:19 +0100

    Update qt4 session
    Set correct path and fix include and libdir settings during build

--- qt4/rules.py
+++ qt4/rules.py
@@ -1,7 +1,7 @@
 import os.path
 import jam.session
 
-from jam.system import Command, Make, Copy
+from jam.system import Command, Make, Copy, Quilt, Mkdirs, Move
 
 class Qt4(jam.session.ConfigureSession):
 
@@ -11,16 +11,24 @@ class Qt4(jam.session.ConfigureSession):
     version = "4.8.0"
     name = "qt4"
 
+    patchsystem = Quilt
+
     src_path = "%(src_dir)s/qt-everywhere-opensource-src-%(version)s"
 
     depends = ["dbus",
                "sqlite3",
+               "mysql",
                "libpng",
                "libjpeg",
                "libtiff",
                "openssl",
                "pkg-config",
                "zlib"]
+
+    configure_cpath = ["%(prefix)s/include"]
+    configure_library_path = ["%(prefix)s/lib"]
+    build_cpath = configure_cpath
+    build_library_path = configure_library_path
 
     configure_args = ["-opensource",
                       "-confirm-license",
@@ -36,30 +44,38 @@ class Qt4(jam.session.ConfigureSession):
                       "-no-framework",
                       "-cocoa",
                       "-prefix %(prefix)s",
-                      "-L%(prefix)s/lib",
-                      "-I%(prefix)s/include",
                       "-L/usr/X11/lib",
                       "-I/usr/X11/include",
                       "-system-libpng",
                       "-system-zlib",
                       "-system-sqlite",
+                      "-plugin-sql-mysql",
                       "-demosdir %(prefix)s/share/%(name)s/demos",
                       "-examplesdir %(prefix)s/share/%(name)s/examples",
                       "-translationdir %(prefix)s/share/%(name)s/translations",
                       "-docdir %(prefix)s/share/%(name)s/doc",
+                      "-plugindir %(prefix)s/lib/%(name)s/plugins",
+                      "-importdir %(prefix)s/lib/%(name)s/imports",
+                      "-datadir %(prefix)s/share/%(name)s",
+                      "-sysconfdir %(prefix)s/etc/xdg",
                       ]
 
-    #TODO move *.app from prefix/bin to apps_dir
 
     def configure(self):
         if self.config.get("verbose"):
             self.configure_args.append("-v")
-        cmd = os.path.join(self.src_path, "configure")
-        Command(cmd, self.configure_args, self.build_path, self.debug).run()
+        cmd_str = os.path.join(self.src_path, "configure")
+        cmd = Command(cmd_str, self.configure_args, self.build_path, self.debug)
+        cmd.set_env("CPATH", ":".join(self.configure_cpath))
+        cmd.set_env("LIBRARY_PATH", ":".join(self.configure_library_path))
+        cmd.run()
+
 
     def destroot(self):
         args = ["INSTALL_ROOT=" + self.dest_dir, "install"]
-        Make(self.build_path, self.debug).run(args)
+        make = Make(self.build_path, self.debug).run(args)
         Copy(os.path.join(self.src_path, "src", "gui", "mac", "qt_menu.nib"),
-             os.path.join(self.dest_path, "lib", "Resources", "qt_menu.nib")
-             ).run()
+             os.path.join(self.dest_path, "lib", "Resources")).run()
+        Mkdirs(self.apps_dir).run()
+        Move(self.dest_path + "/bin/*.app",
+             self.destroot_path + self.apps_dir).run()
