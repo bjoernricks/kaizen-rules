1e09ad4b967b06717eb7ec6470b1375f1e660e87
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-04-12 22:08:11 +0200

    Add session for fontconfig

---
+++ fontconfig/rules.py
@@ -0,0 +1,13 @@
+import jam.session
+
+class Fontconfig(jam.session.ConfigureSession):
+
+    url = "http://www.freedesktop.org/software/fontconfig/release/fontconfig-%(version)s.tar.gz"
+    hash = { "md5" : "26c83855ed256b5c032baae032fadc4f",
+             "sha1" : "1ab2f437c2261028ae7969892277af2d8d8db489" }
+    version = "2.9.0"
+    name = "fontconfig"
+
+    depends = ["libxml2", "freetype", "libiconv", "pkg-config"]
+
+    configure_args = ["--enable-libxml2"]
