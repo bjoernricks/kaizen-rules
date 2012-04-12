4ba7c75c2d8fb36e94ca296958031abe51291dac
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-04-12 22:08:58 +0200

    Add session for eigen

---
+++ eigen/rules.py
@@ -0,0 +1,13 @@
+import jam.session
+
+class Eigen(jam.session.CMakeSession):
+
+    url = "http://bitbucket.org/eigen/eigen/get/%(version)s.tar.bz2"
+    hash = { "md5" : "43070952464a5bf21694e082e7fb8fce",
+             "sha1" : "f2c726897860b4ebc63f74f6097d315aacee1eaf" }
+    version = "3.0.5"
+    name = "eigen"
+
+    src_path = "%(src_dir)s/eigen-eigen-6e7488e20373"
+
+    depends = ["qt4", "pkg-config"]
