734b18f4924d221a7013a9ce3ca028795de329b6
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-04-13 21:36:57 +0200

    Add rule for pyliblzma
    pyliblzma is used to decompress xz archives.

---
+++ python-pyliblzma/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Pythonpyliblzma(jam.session.PythonSession):
+
+    url =  "http://pypi.python.org/packages/source/p/pyliblzma/pyliblzma-%(version)s.tar.bz2"
+    hash = { "md5" : "500f61116ee1ab4063b49c121786863a",
+             "sha1" : "6240ec6f830f35f4087b8926a95c2074320b7ed5" }
+    version = "0.5.3"
+    name = "pyliblzma"
+
+    depends = ["xz"]
