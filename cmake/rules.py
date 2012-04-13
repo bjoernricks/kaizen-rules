07f157aa1d004a4c62ca23fb74139018b70f8a92
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-04-13 12:56:05 +0200

    Update cmake to 2.8.7
    Remove obsolete patch and add dependencies.

--- cmake/rules.py
+++ cmake/rules.py
@@ -4,11 +4,13 @@ from jam.system import Delete, Quilt
 
 class Cmake(jam.session.ConfigureSession):
 
-    url = "http://www.cmake.org/files/v2.8/cmake-2.8.5.tar.gz"
-    hash = { "md5" : "3c5d32cec0f4c2dc45f4c2e84f4a20c5",
-             "sha1" : "8dd4c31cbccf1a297829a476a0ef79d1614ca368" }
-    version = "2.8.5"
+    url = "http://www.cmake.org/files/v2.8/cmake-%(version)s.tar.gz"
+    hash = { "md5" : "e1b237aeaed880f65dec9c20602452f6",
+             "sha1" : "d206182b454f4d1ddeb0f11d7d1be8a66e5c2464" }
+    version = "2.8.7"
     name = "cmake"
+
+    depends = ["bzip2", "ncurses", "libcurl", "libexpat", "zlib"]
 
     configure_args = ["--mandir=/share/man", "--docdir=/share/doc/cmake",
                       "--system-libs", "--datadir=/share/cmake",
