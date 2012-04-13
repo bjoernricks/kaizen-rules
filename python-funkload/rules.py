6d2cd4c17346b6af132157a49e46b46a9f443c16
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-04-13 10:25:03 +0200

    Add rules for python-funkload

---
+++ python-funkload/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Pythonfunkload(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/f/funkload/funkload-%(version)s.tar.gz"
+    hash = { "md5" : "d2e5e5098e332c75b02825ed4318e49c",
+             "sha1" : "48ada07dfa951f5dd9bc1555f25fb0b6f0bccd16" }
+    version = "1.16.1"
+    name = "funkload"
+
+    depends = ["python-webunit", "python-docutils", "python-distribute"]
