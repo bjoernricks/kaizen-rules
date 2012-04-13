2caa11802ba3a5cf79c262937d4ba376fdec146a
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-04-13 10:22:14 +0200

    Update git-manpages to 1.7.10

--- git-manpages/rules.py
+++ git-manpages/rules.py
@@ -6,9 +6,9 @@ from jam.system import Copy
 class Gitmanpages(jam.session.Session):
 
     url = "http://git-core.googlecode.com/files/git-manpages-%(version)s.tar.gz"
-    hash = { "md5" : "dfe782bd962ad66a7865c0db27d48362",
-             "sha1" : "93315f7f51d7f27d3e421c9b0d64afa27f3d16df" }
-    version = "1.7.8"
+    hash = { "md5" : "5066600fe67986acd36c008c876e9489",
+             "sha1" : "5852d1dead0190edeba1803a70fac5d76523a616" }
+    version = "1.7.10"
     name = "git-manpages"
 
     def destroot(self):
