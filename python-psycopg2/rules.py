b8f78c19c68dbe4a867c1f4d613bdcadbb00c519
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-04-25 12:09:50 +0200

    Add rules for python psycopg2
    psycopg2 is the postgres dbapi module

---
+++ python-psycopg2/rules.py
@@ -0,0 +1,11 @@
+import jam.session
+
+class Pythonpsycopg2(jam.session.PythonSession):
+
+    url = "http://pypi.python.org/packages/source/p/psycopg2/psycopg2-%(version)s.tar.gz"
+    hash = { "md5" : "639e014ea9ce3aa3306724f12d16d79b",
+             "sha1" : "d8a98dbb4fed1a5871c658d2b35fe8c92293086a" }
+    version = "2.4.4"
+    name = "psycopg2"
+
+    depends = ["postgresql"]
