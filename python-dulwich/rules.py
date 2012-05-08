62b54f25e7a50db6424161292b7fb9d2d5125972
Bjoern Ricks <bjoern.ricks@googlemail.com>
2012-05-08 21:02:04 +0200

    Add rules for python dulwich a git python implemenation

---
+++ python-dulwich/rules.py
@@ -0,0 +1,9 @@
+import jam.session
+
+class Pythondulwich(jam.session.PythonSession):
+
+    url = "http://www.samba.org/~jelmer/dulwich/dulwich-%(version)s.tar.gz"
+    hash = { "md5" : "544724f9ac9fe6f9865526917ad284d9",
+             "sha1" : "b4e55125fd21908df02fa78efc3868a99636a2cb" }
+    version = "0.8.5"
+    name = "dulwich"
