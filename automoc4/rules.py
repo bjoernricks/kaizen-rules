32c278916083fd932ae8f4de1ca63f4b99b42246
Björn Ricks <bjoern.ricks@gmail.com>
2012-09-01 12:52:46 +0200

    Drop unnecessary comments from automoc4 rules

--- automoc4/rules.py
+++ automoc4/rules.py
@@ -7,25 +7,3 @@ class Automoc4(kaizen.rules.CMakeRules):
              "sha1" : "d864c3dda99d8b5f625b9267acfa1d88ff617e3a" }
     version = "0.9.88"
     name = "automoc4"
-
-    # uncomment to set path to source directory
-    # default is %(src_dir)s/%(name)s-%(version)s
-    # src_path = ""
-
-    # uncomment to set path to build directory (normally not necessary)
-    # build_path = ""
-
-    # uncomment to pass additional parameters to cmake
-    # configure_args = [""]
-
-    # uncomment to pass additional parameters to make
-    # build_args = [""]
-
-    # uncomment to add additonal patches 
-    # all patches will be copied to %(downloadroot)s/%(session)s/patches
-    # e.g. patches = ["01-patch.diff", "http://url.com/remotepatch.diff",
-    #                ["http://url.com/abc.diff", "localname.diff"]]
-    # patches = [""]
-
-    # uncomment to add a dependency on an other session
-    # depends = [""]
