44f82a02444639e93cfaf383389d9597c11e335b
Björn Ricks <bjoern.ricks@gmail.com>
2012-09-10 18:53:31 +0200

    Update soprano to 2.8.0 and remove clucene depedency

--- soprano/rules.py
+++ soprano/rules.py
@@ -3,15 +3,16 @@ import kaizen.rules
 class Soprano(kaizen.rules.CMakeRules):
 
     url = "http://downloads.sourceforge.net/project/soprano/Soprano/%(version)s/soprano-%(version)s.tar.bz2"
-    hash = { "md5" : "783fb07f9679f45e987aff7a17bef649",
-             "sha1" : "d16231dc021bd66eebb9d1975ba075d2169a2617" }
-    version = "2.7.4"
+    hash = { "md5" : "273c3403aeb6d8a43e78a4887f50a385",
+             "sha1" : "55882adb68f90ec36b79ecf64206521226e74c8f" }
+    version = "2.8.0"
     name = "soprano"
 
-    depends = ["raptor2", "rasqal", "redland", "clucene", "qt4", "libiodbc",
+    depends = ["raptor2", "rasqal", "redland", "qt4", "libiodbc",
                "strigi"]
 
     runtime_depends = ["virtuoso"]
 
     configure_args = ["-DSOPRANO_DISABLE_SESAME2_BACKEND=ON",
+                      "-DSOPRANO_DISABLE_CLUCENE_INDEX=ON",
                       "-DCMAKE_BUILD_TYPE=Release"]
