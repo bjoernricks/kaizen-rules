efca2f5ed3375c72cf8ea1c78c9dbdbbd426c4ca
Björn Ricks <bjoern.ricks@gmail.com>
2012-09-10 13:41:34 +0200

    Fix md5 hash for cmake

--- cmake/rules.py
+++ cmake/rules.py
@@ -5,7 +5,7 @@ from kaizen.system import Delete, Quilt
 class Cmake(kaizen.rules.ConfigureRules):
 
     url = "http://www.cmake.org/files/v2.8/cmake-%(version)s.tar.gz"
-    hash = { "md5" : "801f4c87f8b604f727df5bf1f05a59e7",
+    hash = { "md5" : "6464d429b1396a8db35864e791673b65",
              "sha1" : "b96663c0757a5edfbddc410aabf7126a92131e2b" }
     version = "2.8.9"
     name = "cmake"
