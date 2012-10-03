876b528c24e5c3d9616e150e610d70f6b618a883
Björn Ricks <bjoern.ricks@gmail.com>
2012-10-03 17:13:15 +0200

    Update attica to 0.4.1 and use quilt for patches

--- attica/rules.py
+++ attica/rules.py
@@ -1,14 +1,18 @@
 import kaizen.rules
 
+from kaizen.system.patch import Quilt
+
 class Attica(kaizen.rules.CMakeRules):
 
     url = "ftp://ftp.kde.org/pub/kde/stable/attica/attica-%(version)s.tar.bz2"
-    hash = { "md5" : "5a77f678422e7a52e3a3abb2d7ad0499",
-             "sha1" : "ed1d6f49bbc2362464346c4066eb4eaade69a08b" }
-    version = "0.3.0"
+    hash = { "md5" : "b90983ec5d79e5ddcbc9146fa23cab72",
+            "sha1" : "f4962636ec9282c32c4ceaec0c85f92ca5102b60" }
+    version = "0.4.1"
     name = "attica"
 
     depends = ["qt4"]
+
+    patch_cmd = Quilt
 
     patches = ["patch-lib-CMakeLists.txt.diff"]
 
