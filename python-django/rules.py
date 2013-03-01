1413a79afa0010da6e7f88ccccc5f3a6409d3547
Björn Ricks <bjoern.ricks@gmail.com>
2013-03-01 18:32:41 +0100

    Update django to 1.5

--- python-django/rules.py
+++ python-django/rules.py
@@ -4,7 +4,7 @@ class Django(kaizen.rules.PythonRules):
 
     url = ["https://www.djangoproject.com/download/%(version)s/tarball/",
            "django-%(version)s.tar.gz"]
-    hash = { "md5" : "851d00905eb70e4aa6384b3b8b111fb7",
-             "sha1" : "1bfaa4643c6775fbf394137f1533659be45441e7" }
-    version = "1.4.5"
+    hash = { "md5" : "fac09e1e0f11bb83bb187d652a9be967",
+             "sha1" : "358dce7db72904c334e3d7ce7eaa0e27a22cfa16" }
+    version = "1.5"
     name = "Django"
