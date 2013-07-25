0383ee3c68ee950264e9e81830c714ed96f889dd
Bjoern Ricks <bjoern.ricks@intevation.de>
2013-07-25 11:17:13 +0200

    Use version 1.4.5 for django
    Also install bash-completion script for djangos manage.py script.

--- python-django/rules.py
+++ python-django/rules.py
@@ -1,4 +1,7 @@
+import os.path
 import kaizen.rules
+
+from kaizen.system import Copy
 
 class Django(kaizen.rules.PythonRules):
 
@@ -6,5 +9,10 @@ class Django(kaizen.rules.PythonRules):
            "django-%(version)s.tar.gz"]
     hash = { "md5" : "fac09e1e0f11bb83bb187d652a9be967",
              "sha1" : "358dce7db72904c334e3d7ce7eaa0e27a22cfa16" }
-    version = "1.5"
+    version = "1.4.5"
     name = "Django"
+
+    def post_destroot(self):
+        Copy(os.path.join(self.build_path, "extras", "django_bash_completion"),
+             os.path.join(self.dest_path, "etc",
+                          "bash_completion.d", "django_bash_completion")).run()
