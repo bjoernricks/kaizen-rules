ebe4117151b2df7c3201774ebd9350763f7c9802
Björn Ricks <bjoern.ricks@gmail.com>
2014-01-24 12:58:46 +0100

    Update python-docutils to version 0.11

--- python-docutils/rules.py
+++ python-docutils/rules.py
@@ -1,9 +1,15 @@
 from kaizen.rules import PythonRules
 
+
 class PythonDocutils(PythonRules):
 
-    url = "http://prdownloads.sourceforge.net/docutils/docutils-0.7.tar.gz?download"
-    version = "0.7"
+    url = "http://pypi.python.org/packages/source/d/docutils/" \
+          "%(name)s-%(version)s.tar.gz"
+    version = "0.11"
     name = "docutils"
+    hash = {
+        "md5": "20ac380a18b369824276864d98ec0ad6",
+        "sha1": "3894ebcbcbf8aa54ce7c3d2c8f05460544912d67",
+    }
 
     src_path = "%(src_dir)s/%(name)s-%(version)s"
